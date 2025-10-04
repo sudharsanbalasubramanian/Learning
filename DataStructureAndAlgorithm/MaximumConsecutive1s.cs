670d87073510e0a48559f1b897100c44095ca116
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2025-10-05 00:00:20 +0530

    4/10/25 learning

---
+++ DataStructureAndAlgorithm/MaximumConsecutive1s.cs
@@ -0,0 +1,30 @@
+﻿using SharedProject.Extensions;
+
+namespace DataStructureAndAlgorithm
+{
+    public partial class ArrayProblems
+    {
+        public static int MaximumConsecutive1s(List<int> list)
+        {
+            list.ThrowIfNullOrEmpty();
+
+            int res = 0;
+            int curRes = 0;
+
+            foreach (var value in list)
+            {
+                if (value == 0)
+                {
+                    curRes = 0;
+                }
+                else
+                {
+                    curRes++;
+                    res = Math.Max(curRes, res);
+                }
+            }
+
+            return res;
+        }
+    }
+}
