d64d46f5a4249f2a171be44c10f3c3b5b9f8b4bb
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2025-10-21 17:11:55 +0530

    EquilibriumPoint 21-10-25

---
+++ DataStructureAndAlgorithm/Arrays/EquilibriumPoint.cs
@@ -0,0 +1,30 @@
+﻿using SharedProject.Extensions;
+
+namespace DataStructureAndAlgorithm.Arrays
+{
+    public partial class ArrayProblems
+    {
+
+        public static int EquilibriumPoint(List<int> values)
+        {
+            values.ThrowIfNullOrEmpty(nameof(values));
+
+            int totalSum = values.Sum();
+            int leftSum = 0;
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                int rightSum = totalSum - leftSum - values[i];
+
+                if (leftSum == rightSum)
+                {
+                    return i; // Equilibrium index found
+                }
+
+                leftSum += values[i];
+            }
+
+            return -1; // No equilibrium index
+        }
+    }
+}
---
+++ SharedProject/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedProject.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T>? source)
+        {
+            return source == null || !source.Any();
+        }
+        public static void ThrowIfNullOrEmpty<T>(this IEnumerable<T>? source, string paramName)
+        {
+            if (source == null)
+                throw new ArgumentNullException(paramName, $"{paramName} cannot be null.");
+
+            if (!source.Any())
+                throw new ArgumentException($"{paramName} cannot be empty.", paramName);
+        }
+    }
+}
