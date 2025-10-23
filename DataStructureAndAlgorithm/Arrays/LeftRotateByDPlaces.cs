edfe75793b305afd7a1ab628bb2d4e86a89888f4
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2025-10-23 17:20:20 +0530

    Left rotate by d places 23/10/25

---
+++ DataStructureAndAlgorithm/Arrays/LeftRotateByDPlaces.cs
@@ -0,0 +1,25 @@
+﻿using SharedProject.Extensions;
+
+namespace DataStructureAndAlgorithm.Arrays
+{
+    public partial class ArrayProblems
+    {
+        public static void LeftRotate<T>(List<T> values, int d)
+        {
+            values.ThrowIfNullOrEmpty(nameof(values));
+
+            int n = values.Count;
+            d = d % n; // handle d > n
+
+            // Step 1: Reverse first d elements
+            values.Reverse(0, d);
+
+            // Step 2: Reverse remaining n - d elements
+            values.Reverse(d, n - d);
+
+            // Step 3: Reverse the whole list
+            values.Reverse();
+        }
+
+    }
+}
---
+++ DataStructureAndAlgorithm/Arrays/StockBuyAndSell.cs
@@ -0,0 +1,27 @@
+﻿using SharedProject.Extensions;
+
+namespace DataStructureAndAlgorithm.Arrays
+{
+    public partial class ArrayProblems
+    {
+        public static int StockBuyAndSell(List<int> list)
+        {
+            list.ThrowIfNullOrEmpty(nameof(list));
+
+            var totalProfit = 0;
+
+            for (int i = 1; i < list.Count; i++)
+            {
+                var profit = list[i] - list[i - 1];
+
+                if(profit > 0)
+                {
+                    totalProfit += profit;
+                }
+            }
+
+            return totalProfit;
+        }
+
+    }
+}
