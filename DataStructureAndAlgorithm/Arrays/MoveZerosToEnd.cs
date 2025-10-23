14882ec216d6e4cc6c8ce458403a2ffcad86ca8e
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2025-10-23 18:47:11 +0530

    Move zeros to end 23-10-25

---
+++ DataStructureAndAlgorithm/Arrays/MoveZerosToEnd.cs
@@ -0,0 +1,24 @@
+﻿using SharedProject.Extensions;
+
+namespace DataStructureAndAlgorithm.Arrays
+{
+    public partial class ArrayProblems
+    {
+        public static void MoveZeroToEnd(List<int> values)
+        {
+            values.ThrowIfNullOrEmpty(nameof(values));
+
+            int countOfZero = 0;
+
+            for(int i = 0; i < values.Count; i++)
+            {
+                if(values[i] != 0)
+                {
+                    (values[i], values[countOfZero]) = (values[countOfZero], values[i]);
+                    countOfZero += 1;
+                }
+            }
+
+        }
+    }
+}
