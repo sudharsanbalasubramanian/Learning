f48bfd9ba9c011f22197acd30f1000a91bb4bda5
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2025-10-23 14:27:04 +0530

    Array problems 23/10/25

---
+++ SharedProject/Extensions/IntegerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedProject.Extensions
+{
+    public static class IntegerExtensions
+    {
+        public static bool IsEven(this int value)
+        {
+            return value % 2 == 0;
+        }
+
+        public static bool IsOdd(this int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
