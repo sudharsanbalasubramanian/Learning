8b5ed2ef6ee3a3a77a5527fe907146d583449457
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2025-10-24 16:51:06 +0530

    Search first index and last index 24-10-25

---
+++ DataStructureAndAlgorithm/Searching/IndexofFirstOccurrenceinSorted.cs
@@ -0,0 +1,41 @@
+﻿using SharedProject.Extensions;
+
+namespace DataStructureAndAlgorithm.Searching
+{
+    public partial class Searching
+    {
+        public static int FirstOccurrence(List<int> numbers, int val)
+        {
+            numbers.ThrowIfNullOrEmpty(nameof(numbers));
+
+            int low = 0;
+            int high = numbers.Count - 1;
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (numbers[mid] > val)
+                {
+                    high = mid - 1;
+                }
+                else if (numbers[mid] < val)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    if (mid == 0 || numbers[mid - 1] != numbers[mid])
+                    {
+                        return mid;
+                    }
+                    else
+                        high = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
---
+++ DataStructureAndAlgorithm/Searching/IndexofLastOccurrenceinSorted.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAndAlgorithm.Searching
+{
+    public partial class Searching
+    {
+        public static int LastOccurrence(List<int> numbers, int val)
+        {
+            int low = 0;
+            int high = numbers.Count - 1;
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (numbers[mid] == val)
+                {
+                    if (mid == numbers.Count - 1 || numbers[mid + 1] != numbers[mid])
+                    {
+                        return mid;
+                    }
+                    else
+                    {
+                        low = mid + 1; // Continue searching right
+                    }
+                }
+                else if (numbers[mid] > val)
+                {
+                    high = mid - 1; // Move left ✅
+                }
+                else
+                {
+                    low = mid + 1; // Move right ✅
+                }
+            }
+
+            return -1; // Better than default
+        }
+
+    }
+}
