289a6c89ec8f3fa7451d43222b44a5eb49e3bc63
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2025-10-26 22:48:14 +0530

    26-10-25 Valid anagram

---
+++ DataStructureAndAlgorithm/Arrays/ContainsDuplicate.cs
@@ -0,0 +1,25 @@
+﻿using SharedProject.Extensions;
+
+namespace DataStructureAndAlgorithm.Arrays
+{
+    public partial class ArrayProblems
+    {
+        //https://leetcode.com/problems/contains-duplicate/description/
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            nums.ThrowIfNullOrEmpty(nameof(nums));
+
+            HashSet<int> visited = [];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!visited.Add(nums[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
