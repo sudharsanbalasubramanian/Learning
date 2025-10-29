f511d087ed668d286c95ea476b8149eb46a9e8ea
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2025-10-29 15:58:46 +0530

    fibonacci probelm 29-10-25

--- DataStructureAndAlgorithm/DynamicProgramming/FibonacciMemoization.cs
+++ DataStructureAndAlgorithm/DynamicProgramming/FibonacciMemoization.cs
@@ -2,7 +2,7 @@
 {
     public partial class DynamicProgrammingProblems
     {
-        public static Dictionary<int, int> _memo = [];
+        public static readonly Dictionary<int, int> _memo = [];
         public static int Fibonacci(int n)
         {
             if(_memo.TryGetValue(n, out int val))
@@ -10,7 +10,7 @@
                 return val;
             }
 
-            if(n == 0)
+            if(n == 0 || n == 1)
             {
                 _memo[n] = n;
 
---
+++ DataStructureAndAlgorithm/DynamicProgramming/FibonacciTabulationSpaceOptimized.cs
@@ -0,0 +1,29 @@
+﻿namespace DataStructureAndAlgorithm.DynamicProgramming
+{
+    public partial class DynamicProgrammingProblems
+    {
+        public static int FibonacciTabulationSpaceOptimized(int n)
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThan(n, 0);
+
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+
+            int prev2 = 0;
+            int prev1 = 1;
+
+            for(int i = 2; i < n; i++)
+            {
+                var cur = prev2 + prev1;
+
+                prev2 = prev1;
+                prev1 = cur;
+            }
+
+            return prev1;
+        }
+    }
+}
