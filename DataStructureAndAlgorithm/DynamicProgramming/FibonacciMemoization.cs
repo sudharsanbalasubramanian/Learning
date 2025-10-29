68f126697ba903158ddc1b06fcc413c5bc858450
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2025-10-29 15:26:54 +0530

    Fibonacci by memoization

---
+++ DataStructureAndAlgorithm/DynamicProgramming/FibonacciMemoization.cs
@@ -0,0 +1,25 @@
+﻿namespace DataStructureAndAlgorithm.DynamicProgramming
+{
+    public partial class DynamicProgrammingProblems
+    {
+        public static Dictionary<int, int> _memo = [];
+        public static int Fibonacci(int n)
+        {
+            if(_memo.TryGetValue(n, out int val))
+            {
+                return val;
+            }
+
+            if(n == 0)
+            {
+                _memo[n] = n;
+
+                return n;
+            }
+
+            _memo[n] = Fibonacci(n - 1) + Fibonacci(n - 2);
+
+            return _memo[n];
+        }
+    }
+}
