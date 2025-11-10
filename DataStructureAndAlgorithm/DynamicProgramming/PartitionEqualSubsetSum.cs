5a38756c415624b31aed6927d6c8bbd611245f62
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2025-11-10 22:26:06 +0530

    10-11-25

---
+++ DataStructureAndAlgorithm/DynamicProgramming/PartitionEqualSubsetSum.cs
@@ -0,0 +1,18 @@
+﻿using SharedProject.Extensions;
+
+namespace DataStructureAndAlgorithm.DynamicProgramming
+{
+    public partial class DynamicProgrammingProblems
+    {
+        public static bool PartitionEqualSubsetSum(List<int> list, int target)
+        {
+            var sum = list.Sum();
+
+            if(sum.IsOdd())
+            {
+                return false;
+            }
+            return SubsetSumEqualsToTargetSpaceOptimized(list, sum / 2);
+        }
+    }
+}
